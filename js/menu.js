b4913e36c5577341ac31392dcf78a3ac30ef55fb
tatomesa <seba_mesa@hotmail.com>
2025-10-24 16:14:15 -0300

    menu hamburgueza funcionando en todos los html

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const navUl = document.querySelector("nav ul");
+
+  hamburger.addEventListener("click", () => {
+    navUl.classList.toggle("active");
+  });
+});
